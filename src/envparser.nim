355703a8ca92e1800fbcd243147714094e54670c
xTrayambak <xtrayambak@disroot.org>
2025-03-25 13:05:36 +0530

    fix: get rid of AI slop

--- src/envparser.nim
+++ src/envparser.nim
@@ -1,31 +1,21 @@
-#thefunny (also mostly ai generated code)
-
 import std/[os, logging]
 
-type
-  XdgEnv* = object
-    runtimeDir*: string
-    waylandDisplay*: string
-    user*: string
+type XdgEnv* = object
+  runtimeDir*: string
+  waylandDisplay*: string
+  user*: string
+  equinoxPath*: string
 
 proc getXdgEnv*(): XdgEnv =
-  result = XdgEnv(runtimeDir: "", waylandDisplay: "", user: "")
+  let equinoxPath =
+    when defined(release):
+      findExe("equinox")
+    else:
+      getCurrentDir() / "equinox"
 
-  try:
-    result.runtimeDir = getEnv("XDG_RUNTIME_DIR")
-  except KeyError:
-    discard
-
-  try:
-    result.waylandDisplay = getEnv("WAYLAND_DISPLAY")
-  except KeyError:
-    discard
-
-  try:
-    result.user = getEnv("USER")
-  except KeyError:
-    discard
-
-  #Added to check if XDG_RUNTIME_DIR contains non-ASCII characters, and if it does, default to /tmp
-  #[if result.runtimeDir.len > 0 and not result.runtimeDir.allCharsInSet("Charset" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"):
-      result.runtimeDir = "/tmp" ]#
+  XdgEnv(
+    runtimeDir: getEnv("XDG_RUNTIME_DIR"),
+    waylandDisplay: getEnv("WAYLAND_DISPLAY"),
+    user: getEnv("USER"),
+    equinoxPath: equinoxPath
+  )
