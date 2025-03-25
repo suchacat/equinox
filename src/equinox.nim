c736f94a16a2f01f555e5f817ab582d9005b048a
xTrayambak <xtrayambak@disroot.org>
2025-03-25 16:13:12 +0530

    ref: remove all developer only features
    We're open source now so it doesn't really matter

--- src/equinox.nim
+++ src/equinox.nim
@@ -14,22 +14,6 @@ const Splashes = [
   "compiled with the full clanger soyboy toolchain, complete with mimalloc",
 ]
 
-template developerOnly(body: untyped) =
-  ## Professional anti-skidding tool.
-  ## Wrap code blocks in this when you don't want potential skids to access something that can cause them to modify Roblox or do stupid shit in general.
-  ## Beware that it lets them know that the codepath actually exists. Albeit that isn't very useful given the compiler will elide that unreachable code path at preprocessing
-  ## time anyway.
-  ##
-  ## Example:
-  ## .. code-block::Nim
-  ##   developerOnly:
-  ##     disableAPKSignatureVerification()
-  when not defined(release):
-    body
-  else:
-    error "This feature is only available in developer builds."
-    quit(1)
-
 proc showHelp(code: int = 0) {.noReturn.} =
   echo """
 Usage: equinox [mode] [arguments] ...
@@ -46,7 +30,6 @@ Modes:
     start              Start the networking bridge. This is already called by `run` upon starting up.
     stop               Stop the networking bridge. This is already called by `run` upon exiting.
 
-Developer Modes (ONLY AVAILABLE IN INTERNAL BUILDS):
   fetch-image-pair     Fetch a suitable image pair (system+vendor) from the Waydroid OTA
   shell                Run a shell command in the Android container
   sh                   Run a shell REPL in the Android container
@@ -103,96 +86,88 @@ EquinoxHQ is not responsible for any of your actions.
     info "equinox: downloading packages"
     downloadApks(packages, input)
   of "remove-app":
-    developerOnly:
-      if input.arguments.len < 1:
-        error "equinox: expected 1 argument, got none."
-        error "equinox: Run equinox --help for more information."
-        quit(1)
+    if input.arguments.len < 1:
+      error "equinox: expected 1 argument, got none."
+      error "equinox: Run equinox --help for more information."
+      quit(1)
 
-      var platform = getIPlatformService()
-      platform.removeApp(input.arguments[0])
+    var platform = getIPlatformService()
+    platform.removeApp(input.arguments[0])
   of "run":
-    randomize()
     echo "Splash: " & sample(Splashes)
 
     initNetworkService()
     startAndroidRuntime(input)
   of "shell":
-    developerOnly:
-      if input.arguments.len < 1:
-        error "Usage: `equinox shell \"your command goes here\"`"
-        quit(1)
+    if input.arguments.len < 1:
+      error "Usage: `equinox shell \"your command goes here\"`"
+      quit(1)
 
-      let output = runCmdInContainer(input.arguments[0])
-      if *output:
-        echo &output
-      else:
-        info "Command returned no output."
+    let output = runCmdInContainer(input.arguments[0])
+    if *output:
+      echo &output
+    else:
+      info "Command returned no output."
   of "help":
     showHelp()
   of "get-property":
-    developerOnly:
-      if input.arguments.len < 1:
-        error "Usage: `equinox get-property your.property.name`"
-        quit(1)
+    if input.arguments.len < 1:
+      error "Usage: `equinox get-property your.property.name`"
+      quit(1)
 
-      if input.arguments.len == 1:
-        let prop = getProp(input.arguments[0])
-        if *prop:
-          echo &prop
-        else:
-          error "No such property: " & input.arguments[0]
+    if input.arguments.len == 1:
+      let prop = getProp(input.arguments[0])
+      if *prop:
+        echo &prop
       else:
-        for property in input.arguments:
-          let value = getProp(property)
-          if not *value:
-            error "No such property: " & property
-            quit(1)
+        error "No such property: " & input.arguments[0]
+    else:
+      for property in input.arguments:
+        let value = getProp(property)
+        if not *value:
+          error "No such property: " & property
+          quit(1)
 
-          styledWriteLine(
-            stdout,
-            fgCyan,
-            property,
-            resetStyle,
-            styleBright,
-            ": ",
-            resetStyle,
-            fgGreen,
-            &value,
-            resetStyle,
-          )
+        styledWriteLine(
+          stdout,
+          fgCyan,
+          property,
+          resetStyle,
+          styleBright,
+          ": ",
+          resetStyle,
+          fgGreen,
+          &value,
+          resetStyle,
+        )
   of "fetch-image-pair":
-    developerOnly:
-      print getImages()
+    print getImages()
   of "halt":
     stopLxcContainer(
       force = input.enabled("force", "F") or input.enabled("my-time-has-value")
     )
   of "sh":
-    developerOnly:
-      var noise = Noise.init()
-      let prompt = Styler.init(fgGreen, "sh", resetStyle, "> ")
-      noise.setPrompt(prompt)
+    var noise = Noise.init()
+    let prompt = Styler.init(fgGreen, "sh", resetStyle, "> ")
+    noise.setPrompt(prompt)
 
-      while true:
-        let ok = noise.readLine()
-        if not ok:
-          break
+    while true:
+      let ok = noise.readLine()
+      if not ok:
+        break
 
-        let line = noise.getLine()
-        if line == ".quit":
-          break
-        else:
-          let output = runCmdInContainer(line)
-          if *output:
-            echo &output
+      let line = noise.getLine()
+      if line == ".quit":
+        break
+      else:
+        let output = runCmdInContainer(line)
+        if *output:
+          echo &output
   of "get-gsf-id":
-    developerOnly:
-      echo getGSFAndroidID()
+    echo getGSFAndroidID()
   of "launch-app":
-    developerOnly:
-      var platform = getIPlatformService()
-      platform.launchApp(input.arguments[0])
+    var platform = getIPlatformService()
+    platform.launchApp(input.arguments[0])
   of "net":
     if input.arguments.len < 1:
       error "equinox: `net` expects a subcommand (`start`, `stop`)"
