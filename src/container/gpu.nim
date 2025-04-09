42326a537e7ba6d61eae401a65964125988d60bd
xTrayambak <xtrayambak@disroot.org>
2025-04-09 20:42:59 +0530

    experiment: gpu: use dedicated one if possible
    Disabled by default for now.

--- src/container/gpu.nim
+++ src/container/gpu.nim
@@ -1,4 +1,4 @@
-import std/[os, logging, options, strutils, sequtils, tables]
+import std/[algorithm, os, logging, options, strutils, sequtils, tables]
 import pkg/[glob]
 
 const UnsupportedGPUDrivers* = [
@@ -44,12 +44,19 @@ proc getCardFromRenderNode*(device: string): string =
   "/dev" / "dri" / matches[0].splitPath().tail
 
 proc getDriNode*(): Option[DRINode] =
-  for node in glob("/dev/dri/renderD*").walkGlob:
+  when defined(equinoxTrayExperimentDedicatedNodes):
+    let nodes = glob("/dev/dri/renderD*").walkGlob.toSeq.sortedByIt(
+      (char) (((uint8) it[it.len - 1]) - ((uint8) '0'))
+    )
+  else:
+    let nodes = glob("/dev/dri/renderD*").walkGlob.toSeq()
+
+  for node in nodes: 
     let split = splitPath(node).tail
     let renderDev = split
 
     if getKernelDriver(renderDev) notin UnsupportedGPUDrivers:
-      debug "container/gpu: found supported (?) DRI node: " & renderDev
+      debug "container/gpu: found supported DRI node: " & renderDev
       return some(DRINode(dev: node, gpu: getCardFromRenderNode(renderDev)))
     else:
       warn "container/gpu: found unsupported DRI node: " & renderDev
