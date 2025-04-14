93431d1760bb5b3b84ab78bc64c74cce6e5a06ed
xTrayambak <xtrayambak@disroot.org>
2025-04-14 13:10:57 +0530

    fix: better selinux policy buildsystem

--- build/selinux.sh
+++ build/selinux.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env sh
 
+if [[ "$1" == "devel" ]]; then
+	sed "s|%bin|$(pwd)/equinox|g" equinox.fc.template > equinox.fc
+else
+	sed "s|%bin|/usr/bin/equinox|g" equinox.fc.template > equinox.fc
+fi
+
 # Compile the Equinox SELinux module
 make NAME=targetted -f /usr/share/selinux/devel/Makefile
+
+if [[ "$2" == "install" ]]; then
+	sudo semodule -i equinox.pp
+fi
