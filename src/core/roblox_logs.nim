fa31b6fa2478e2b6b6e28c9aca1e76f838aaa516
Trayambak Rai <xtrayambak@disroot.org>
2025-04-24 21:36:02 +0530

    fix: roblox_logs: wait for roblox state manager to hit STOP before registering exit

--- src/core/roblox_logs.nim
+++ src/core/roblox_logs.nim
@@ -52,7 +52,7 @@ proc checkLineForEvents*(line: string) =
       warn "malformed BloxstrapRPC payload received: " & exc.msg
       warn "this game is sending bad payloads, it seems like."
       return
-  elif line.contains("Warning [FLog::Graphics] RenderView destroyed"):
+  elif line.contains("setTaskSchedulerBackgroundMode() enable:true context:ASMA.stop"):
     debug "watcher: looks like roblox is stopping."
     chan.send(EventPayload(kind: Event.RobloxClose))
 
