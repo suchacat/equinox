aaada99c933a69349cf724dcdc5328d7ba8404ce
Trayambak Rai <xtrayambak@disroot.org>
2025-04-24 21:35:37 +0530

    add: container: implement settings system

---
+++ src/container/settings.nim
@@ -0,0 +1,19 @@
+## Android settings
+## Copyright (C) 2025 the EquinoxHQ team
+import std/[logging]
+import ./[lxc],
+       ./utils/exec
+
+proc settingsPut*(namespace: string, key: string, value: bool | string | SomeInteger) =
+  var cmd = "/bin/cmd settings put " & namespace & ' ' & key & ' '
+
+  when value is bool:
+    cmd &= $int(value)
+
+  when value is string:
+    cmd &= '"' & value & '"'
+
+  when value is SomeInteger:
+    cmd &= $value
+  
+  discard runCmdInContainer(move(cmd))
